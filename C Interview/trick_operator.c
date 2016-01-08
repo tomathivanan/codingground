3a1ec08085b255ce393b3cec47b76d05902a935a
Mathivanan <tomathivanan@users.noreply.github.com>
2016-01-09 01:37:07 +0530

    Created by Coding Ground Client

---
+++ C Interview/trick_operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#if 0
+#include "test.z"
+main()
+{
+    int i = 10;
+    while(i --> 0)
+    {
+     printf("%d \r\n", i);
+     printf("\r\n%s", TEST_VAR);
+    }
+}
+
+#endif
